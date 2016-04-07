5e848d7f47d59cc69b633588c023ce61d1638d84
swapnull <martyn.e.rushton@gmail.com>
2016-04-08 00:09:08 +0100

    removed anemoi as NewPing can be used instead

--- olympus/src/sketch.ino
+++ olympus/src/sketch.ino
@@ -9,38 +9,44 @@ void setup()
     Serial.begin(9600);
     
     //motors
-    //int motors[1][2] = {{25, 4}};//, {24, 5}, {23, 6}, {22, 7}};
-    //zeus.initHermes(motors);
+    int motors[4][2] = {{25, 8}, {24, 9}, {23, 10}, {22, 11}};
+    zeus.initHermes(motors);
+
+    attachInterrupt(digitalPinToInterrupt(2), turningInterrupt ,CHANGE);
+    attachInterrupt(digitalPinToInterrupt(3), turningInterrupt ,CHANGE);
+    attachInterrupt(digitalPinToInterrupt(18), turningInterrupt ,CHANGE);
+    attachInterrupt(digitalPinToInterrupt(19), turningInterrupt ,CHANGE);
+
     //zeus.hermes.moveForward(200);
 
 
     //sonar
-    //zeus.addSonar(8, 9, 200);
-   // zeus.hermes.moveForward(200);
-    //delay(500);
-    //Serial.println(zeus.sonar[FRONT].ping_cm());
-
-    /*zeus.hermes.turn(CLOCKWISE, 90, 50);
-    delay(1000);
-    zeus.hermes.turn(ANTICLOCKWISE, 90, 50);*/
-    //zeus.hermes.stop();
-    //zeus.hermes.moveForward(100);
-    //zeus.avoid();
+    zeus.addSonar(37, 36, 200);
 
 }
 
 void loop()
 {
-    zeus.runThea();
-
-    /*if(analogRead(4) > 500){
-        zeus.hermes.moveForward(200);
+    if(analogRead(4) > 500){
+        zeus.avoid();
+        zeus.hermes.moveForward(50);
     }else{
         zeus.hermes.stop();
-    }*/
+    }
+    delay(500);
+}
 
-    //Serial.println(zeus.sonar[FRONT].ping_cm());
-    //delay(1000);
-    //zeus.hermes.moveForward(100);
-    //zeus.avoid();
+volatile int _steps = 0;
+
+void turningInterrupt(){
+    Serial.print("steps: ");
+    Serial.println(_steps);
+    _steps++;
+
+    if(_steps % 90 == 0){
+        Serial.println("stop turning");
+        zeus.hermes.stop();
+    }else{
+        zeus.hermes.turning = true;
+    }
 }
